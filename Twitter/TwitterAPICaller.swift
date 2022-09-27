10f0edf28d4bd3e2ee46c52b2670e4311c5b9962
Tasneem Hasanat <tasneemhasanat97@gmail.com>
2022-09-26 21:53:01 -0400

    added twitter API and build logic to code

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "8sx0whXcfO6I6kR8uiT5Kj6iK", consumerSecret: "I9d3ni6mKo817m7nElThiKs6qo8eOEjcF2nTxHukj34PxOYkRi")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
